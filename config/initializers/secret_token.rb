b1e6eb88f2c0260df58b7e1a31f2d134df96917a
bthews <bthews@gmail.com>
2014-10-15 07:13:17 -0400

    Initial 710 app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App710::Application.config.secret_key_base = '9538d9974c406c710a00e7bcf487dbcff5c7df78acff2b9142e5e25e7d6dc7a8fc64fc92a513af220b62f2e0004ba5b8817cc64163d1fc25b475e94815aae0a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+App710::Application.config.session_store :cookie_store, key: '_app_710_session'
